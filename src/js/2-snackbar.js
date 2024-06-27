6bfd153f27cbc956404fd76ac3e3f7330ae6b023
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-27 09:51:56 +0300

    summ 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,5 @@
 //2 js
+// `✅ Fulfilled promise in ${delay}ms` (Якщо проміс виконується вдало, виводь у консоль наступний рядок, де delay — це значення затримки виклику промісу в мілісекундах.)
+// `❌ Rejected promise in ${delay}ms` (Якщо проміс буде відхилено, то виводь у консоль наступний рядок, де delay — це значення затримки промісу в мілісекундах.)
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
