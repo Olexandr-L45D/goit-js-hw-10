af3c4e05e417c481e1926450b4b804be5ed51682
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-28 08:19:00 +0300

    3 mon

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,69 +1,69 @@
 
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatPickr.min.css";
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatPickr.min.css";
 // import "flatpickr/dist/flatpickr.min.css";
 
 // import iziToast from "izitoast";
 // import "izitoast/dist/css/iziToast.min.css";
 
-const elementses = {
-second: document.querySelector('.clock-seconds'),
-minute: document.querySelector('.clock-minutes'),
-hour: document.querySelector('.clock-hours'),
-day: document.querySelector('.clock-days')
-};
-const currentTeme = new Date();
-let second = currentTeme.getSeconds();
-let minute = currentTeme.getMinutes();
-let hour = currentTeme.getHours();
-let day = currentTeme.getDay();
+// const elementses = {
+// second: document.querySelector('.clock-seconds'),
+// minute: document.querySelector('.clock-minutes'),
+// hour: document.querySelector('.clock-hours'),
+// day: document.querySelector('.clock-days')
+// };
+// const currentTeme = new Date();
+// let second = currentTeme.getSeconds();
+// let minute = currentTeme.getMinutes();
+// let hour = currentTeme.getHours();
+// let day = currentTeme.getDay();
 
-elementses.second.textContent = second;
-elementses.minute.textContent = minute;
-elementses.hour.textContent = hour;
-elementses.day.textContent = arrDay[day];
+// elementses.second.textContent = second;
+// elementses.minute.textContent = minute;
+// elementses.hour.textContent = hour;
+// elementses.day.textContent = arrDay[day];
 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-//let userSelectedDate = onClose() //записати значення обрано дати пысля валыдацыъ в методы onClose()
+// const options = {
+//     enableTime: true,
+//     time_24hr: true,
+//     defaultDate: new Date(),
+//     minuteIncrement: 1,
+//     onClose(selectedDates) {
+//       console.log(selectedDates[0]);
+//     },
+//   };
+// //let userSelectedDate = onClose() //записати значення обрано дати пысля валыдацыъ в методы onClose()
 
-flatpickr(selector, options) {
-// metod onClose() selectedDates[0]
-if (day <= defaultDate ) {
-    alert("Please choose a date in the future")  
-} else {
+// flatpickr(selector, options) {
+// // metod onClose() selectedDates[0]
+// if (day <= defaultDate ) {
+//     alert("Please choose a date in the future")  
+// } else {
     
-} 
-};
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+// } 
+// };
+// function convertMs(ms) {
+//     // Number of milliseconds per unit of time
+//     const second = 1000;
+//     const minute = second * 60;
+//     const hour = minute * 60;
+//     const day = hour * 24;
   
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+//     // Remaining days
+//     const days = Math.floor(ms / day);
+//     // Remaining hours
+//     const hours = Math.floor((ms % day) / hour);
+//     // Remaining minutes
+//     const minutes = Math.floor(((ms % day) % hour) / minute);
+//     // Remaining seconds
+//     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
-    return { days, hours, minutes, seconds };
-  }
+//     return { days, hours, minutes, seconds };
+//   }
   
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+//   // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
   // addLeadingZero(value) {
   //   padStart()
