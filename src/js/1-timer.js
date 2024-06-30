76467e0da5b2c87d5851c4830287ac97bc0e7755
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-30 07:20:26 +0300

    1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,6 +2,9 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -19,27 +22,22 @@ const elementses = {
   day: document.querySelector('#days')
   };
 
-document.querySelector("#datetime-picker").flatpickr(options);
-new flatpickr("#datetime-picker", options);
-
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+  document.querySelector("#datetime-picker").flatpickr(options);
+  new flatpickr("#datetime-picker", options);
 
 const inputClik = document.querySelector('#datetime-picker');
  inputClik.addEventListener('click', handlerButton)
 
-function handlerButton(event) {
+function handlerButton() {
   flatpickr("#datetime-picker", options);
 
 };
-//    .removeAttr('disabled');//роблю не активною
 
 let userSelectedDate = [0]; // (0) = обраний час of uzer метод onClose()
 
 const id = setInterval(() => {
  userSelectedDate -= 1000;
-//  const currentTemer = new Date();
-const currentTeme = new Date();
+ const currentTeme = new Date();
 let second = currentTeme.getSeconds();
 let minute = currentTeme.getMinutes();
 let hour = currentTeme.getHours();
@@ -48,25 +46,7 @@ elementses.second.textContent = second;
 elementses.minute.textContent = minute;
 elementses.hour.textContent = hour;
 elementses.day.textContent = day;
-
-flatpickr(userSelectedDate, options) 
-{    
-  if (currentTeme >= userSelectedDate) {
-      izitoast("Please choose a date in the future")  
-  //передати user - alert
-  }
-  else {
-   return userSelectedDate
-  }
-}
-  if (!userSelectedDate) { 
-    clearInterval(id); 
-  }
 }, 1000);
-
-setTimeout(()=>{
-    clearInterval(id)  
-},userSelectedDate*1000)
 
 function convertMs(ms) {
     const second = 1000;
@@ -79,11 +59,27 @@ function convertMs(ms) {
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
     return { days, hours, minutes, seconds };
   }
-  console.log(convertMs(userSelectedDate));
-
+  console.log(convertMs(handlerButton()));
+  console.log(convertMs(elementses))
+  
+ //    .removeAttr('disabled');//роблю не активною
   // addLeadingZero(value) {
   //   padStart()
   //   // console.log(addLeadingZero());
   // } // додаэ ноль якщо менше 2 х символы додае 0
+  
+// 
+// {    
+//   if (currentTeme >= targetDate) {
+//       izitoast("Please choose a date in the future")  
+//   //передати user - alert
+//   }
+//   else {
+//   //  return userSelectedDate
+//   }
+// }
+//   if (!userSelectedDate) { 
+//     clearInterval(id); 
+//   }
 
-  let izitoast = new iziToast('.container-clock timer', 250);
+  
