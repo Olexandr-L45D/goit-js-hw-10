c835c389d3eec5a11b39f0ec1b81b9c063723541
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-30 21:25:49 +0300

    upd 2 sn bar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,52 +1,31 @@
-//2 js
-// `✅ Fulfilled promise in ${delay}ms` (Якщо проміс виконується вдало, виводь у консоль наступний рядок, де delay — це значення затримки виклику промісу в мілісекундах.)
-// `❌ Rejected promise in ${delay}ms` (Якщо проміс буде відхилено, то виводь у консоль наступний рядок, де delay — це значення затримки промісу в мілісекундах.)
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
 
-// const formData = {
-//     email: "",
-//     message: ""
-// };
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-// const form = document.querySelector('.feedback-form');
-// const localStorageKey = 'feedback-form-state';
+ const form = document.querySelector('.form');
 
-// window.addEventListener('load', function () {
-//     const storedData = JSON.parse(this.localStorage.getItem(localStorageKey));
-//     if (storedData) {
-//         Array.from(form.elements).forEach(function(element) {
-//             if (element.name && storedData[element.name]) {
-//                 element.value = storedData[element.name];
-//                 formData[element.name] = storedData[element.name];
-//             }
-//         });
-//     }
-// });
+form.addEventListener('submit', function (delay) {
+    const isSuccess = delay;
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (isSuccess) {
+      resolve(`✅ Fulfilled promise in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise in ${delay}ms` );
+    }
+  }, 1000);
+});
 
-// form.addEventListener('input', function (event) {
-//     if (event.target.name === 'email' || event.target.name === 'message') {
-//         formData[event.target.name] = event.target.value.trim();
-//         localStorage.setItem(localStorageKey, JSON.stringify(formData));
-//     };
-// });
+promise
+  .then(value => iziToast) // "Success! Value passed to resolve function"
+  .catch(error => iziToast) // "Error! Error passed to reject function"
+  .finally(() => console.log("Promise settled")); // "Promise settled"
+    });
 
-// form.addEventListener('submit', function (event) {
-//     event.preventDefault();
-//     let allFieldsFilled = true;
-//     Array.from(form.elements).forEach(function (element) {
-//         if (element.name && element.value.trim() === '') {
-//             allFieldsFilled = false;
-//             alert('Fill please all fields');
-//             return;
-//         }
-//     });
 
-//     if (allFieldsFilled) {
-//         console.log(formData);
-//         localStorage.removeItem(localStorageKey);
-//         form.reset();
-//         formData.email = ''; //але  як зробити, щоб не очищати
-//         formData.message = ''; //кожне поле окремо 
-//     }
-// });
+form.addEventListener('input', function (event) {
+    if (event.target.name === 'delay' ) {
+         return event.target.value.trim();
+       
+    };
+});
